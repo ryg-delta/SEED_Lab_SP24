3f69573a3944fb36ec4d84e51e5f70257e9d66a1
ryg-delta <pol.rygina@gmail.com>
2024-02-21 11:36:15 -0700

    Created Mini Project main file for python

---
+++ MiniProject/Python_Code/mini_project_main_python.py
@@ -0,0 +1,190 @@
+# Mini Project
+# Polina Rygina and Silje Ostrem
+# SEED Lab Spring 2024
+# How to Run: Execute using the python terminal.
+# Make sure camera and LCD screen are connected
+# Description:
+#
+# References: Computer Vision and Communication Tutorial
+
+import cv2 as cv
+from cv2 import aruco
+import numpy as np
+from time import sleep
+import board
+import adafruit_character_lcd.character_lcd_rgb_i2c as character_lcd
+import threading
+from smbus2 import SMBus
+
+# I2C address of the Arduino, set in Arduino sketch
+ARD_ADDR = 8
+# Initialize SMBus library with I2C bus 1
+i2c = SMBus(1)
+
+"""    command = [ord(i) for i in input_string]
+    i2c.write_i2c_block_data(ARD_ADDR, offset, command)"""
+
+HEIGHT = 480
+WIDTH = 640
+
+halfWidth = WIDTH // 2
+halfHeight = HEIGHT // 2
+
+lcdMsg = "No marker found."
+detectedMarkers = False
+
+
+def initializeCamera():
+    camera = cv.VideoCapture(0)
+    camera.set(cv.CAP_PROP_FRAME_WIDTH, WIDTH)
+    camera.set(cv.CAP_PROP_FRAME_HEIGHT, HEIGHT)
+    return camera
+
+
+# Takes a photo, with delay to stabilize image
+def takePhoto(camera):
+    sleep(0.1)
+    ret, image = camera.read()
+    if not ret:
+        print("Could not capture image from camera!")
+        quit()
+    else:
+        convImg = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+        return convImg
+
+
+# Runs ArUco detection, sets flag if markers were detected
+def arucoDetect(img):
+    global lcdMsg
+    aruco_dict = aruco.getPredefinedDictionary(aruco.DICT_6X6_50)
+    parameters = cv.aruco.DetectorParameters()
+
+    detector = aruco.ArucoDetector(aruco_dict, parameters)
+    corners, ids, rejected = detector.detectMarkers(img)
+
+    if ids is not None:
+        detectedMarkers = True
+        lcdMsg = "Marker detected."
+    else:
+        detectedMarkers = False
+        lcdMsg = "No markers\ndetected."
+
+    return detectedMarkers, ids, corners
+
+
+# Prints to LCD a message based on the detected_marker flag.
+def printToLCD():
+    global lcdMsg
+    global detectedMarkers
+    currentMsg = ""
+    while True:
+        if lcdMsg != currentMsg:
+            lcd.clear()
+            if detectedMarkers:
+                print("Marker detected.")
+                # ******************************
+                # Write new data to the LCD here
+                # ******************************
+                lcd.message = "Marker detected."
+                currentMsg = "Marker detected."
+            else:
+                print("No markers found.")
+                lcd.message = "No markers\ndetected."
+                currentMsg = "No markers\ndetected."
+        sleep(0.1)
+    return
+
+
+def detectQuad(corners):
+    # Gets coordinates from the aruco detector
+    xCoor = corners[0][0][0][0]
+    yCoor = corners[0][0][0][1]
+    # Each quadrant is labeled 0-4, [00, 01][10,11] (in binary)
+    pos = 0
+    # adds the appropriate amount based on position
+    if xCoor >= halfWidth:
+        pos += 1
+    if yCoor >= halfHeight:
+        pos += 2
+    # Returns the final quadrant
+    return (pos)
+
+
+# Capture Video
+
+
+if __name__ == "__main__":
+    lcdColumns = 16
+    lcdRows = 2
+    # Initialise I2C bus.
+    i2c = board.I2C()  # uses board.SCL and board.SDA
+    # Initialise the LCD class
+    lcd = character_lcd.Character_LCD_RGB_I2C(i2c, lcdColumns, lcdRows)
+    lcd.color = [0, 100, 100]
+
+    myThread = threading.Thread(target=printToLCD, args=())
+    myThread.start()
+
+    vidCap = initializeCamera()
+    sleep(.2)
+
+    # The first frame
+    ret, frame = vidCap.read()
+
+    # Show the first frame - ends when '0' is pushed
+    cv.imshow('basicImg', frame)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
+    cv.imshow("Live Video", frame)
+
+    # Initializations of the positons
+    pos = 5
+    oldpos = 5
+
+    # While loop for LIVE video
+    while vidCap.isOpened():
+        # Read in camera data
+        ret, frame = vidCap.read()
+        grayscale = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+
+        # If the image is good
+        if ret:
+            # Add lines to the image & Make it grayescale
+            cv.line(grayscale, (0, halfHeight), (WIDTH, halfHeight), (0, 255, 0), thickness=5)
+            cv.line(grayscale, (halfWidth, 0), (halfWidth, HEIGHT), color=(0, 255, 0), thickness=5)
+            # Call aruco detector
+            detectedMarkers, ids, corners = arucoDetect(frame)
+            # Show the gram
+            cv.imshow("Live Video", grayscale)
+            # If aruco detected, find the corners
+            if detectedMarkers == True:
+                pos = detectQuad(corners)
+                # If the positon has changed, print
+                if oldpos != pos:
+                    oldpos = pos
+                    print(pos)
+        # wait for 1ms
+        cv.waitKey(1)
+
+    # It's over now
+    print("Done NOw!")
+    vidCap.release()
+    cv.destroyAllWindows()
+
+    """
+    while True:
+        convImg = takePhoto(camera)
+        detectedMarkers, ids, corners = arucoDetect(convImg)
+
+        cv.aruco.drawDetectedMarkers(convImg, corners, ids, borderColor=4)
+
+        # Show camera output for debug
+        cv.imshow("overlay", convImg)
+
+        # Exit works only if "overlay" window is selected
+        if cv.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    camera.release()
+    cv.destroyAllWindows()
+    """
