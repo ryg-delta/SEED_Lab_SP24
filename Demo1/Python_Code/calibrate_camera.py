0910322540edaeaff3aed341c30968fcccbd2837
Polina Rygina <103440380+ryg-delta@users.noreply.github.com>
2024-03-11 09:19:49 -0600

    Updated documentation for calibration file.

--- Demo1/Python_Code/calibrate_camera.py
+++ Demo1/Python_Code/calibrate_camera.py
@@ -7,12 +7,12 @@
 # Description: Runs charuco camera calibration to get camera matrix and distance coefficients
 # References:
 # https://github.com/PhotonVision/photonvision/blob/7f09f9e4f5b4237ef4b9dde7fdcb747115315659/photon-server/src/main/resources/calibration/lifecam480p.json#L10
- #https://mecaruco2.readthedocs.io/en/latest/notebooks_rst/Aruco/sandbox/ludovic/aruco_calibration_rotation.html
+# https://mecaruco2.readthedocs.io/en/latest/notebooks_rst/Aruco/sandbox/ludovic/aruco_calibration_rotation.html
 
 import cv2 as cv
 import numpy as np
 
-
+# If different charuco board, adjust as needed.
 aruco_dict = cv.aruco.DICT_6X6_50
 ROWS = 7
 COLS = 5
@@ -21,11 +21,13 @@ MARKER_LENGTH = 0.015
 
 def calibrate():
     dictionary = cv.aruco.getPredefinedDictionary(aruco_dict)
+    # Create board object so it know what to look for
     board = cv.aruco.CharucoBoard((ROWS, COLS), SQUARE_LENGTH, MARKER_LENGTH, dictionary)
     parameters = cv.aruco.DetectorParameters()
     detector = cv.aruco.ArucoDetector(dictionary, parameters)
     charucoCorners = []
     charucoIds = []
+    # Take 10ish images at different angles.
     images = ["0.png", "1.png", "2.png", "3.png", "4.png", "5.png", "6.png", "7.png", "8.png", "9.png"]
 
 
@@ -53,7 +55,7 @@ def calibrate():
                                  [0., 0., 1.]])
 
     distCoeffsInit = np.zeros((5, 1))
-
+    # Use flags, causes weird behavior otherwise
     flags = (cv.CALIB_USE_INTRINSIC_GUESS + cv.CALIB_RATIONAL_MODEL + cv.CALIB_FIX_ASPECT_RATIO)
     #flags = (cv.CALIB_RATIONAL_MODEL) causes mega distortion lmao
     (ret, camMtx, distCoeffs,
@@ -69,9 +71,6 @@ def calibrate():
         flags=flags,
         criteria=(cv.TERM_CRITERIA_EPS & cv.TERM_CRITERIA_COUNT, 10000, 1e-9))
 
-
-    print(camMtx)
-    print(distCoeffs)
     # Save calibration data
     np.save('camera_matrix.npy', camMtx)
     np.save('dist_coeffs.npy', distCoeffs)
