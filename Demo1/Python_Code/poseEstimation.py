250c8043f1b67825838ddb75a14bec70d6955e85
Polina Rygina <103440380+ryg-delta@users.noreply.github.com>
2024-03-11 09:16:43 -0600

    Update documentation.

--- Demo1/Python_Code/poseEstimation.py
+++ Demo1/Python_Code/poseEstimation.py
@@ -1,28 +1,32 @@
+# Demo 1
+# Polina Rygina
+# SEED Lab Spring 2024
+# How to Run: Execute using the python terminal.
+# Make sure either camera is connected for live feed, or you load correct image file.
+# Description:
+# Runs pose estimation on image, can be extended to video feed.
+# References:
+# To do: Move axes drawing code to function?
+
+
 import cv2 as cv
 from cv2 import aruco
 import numpy as np
 from time import sleep
 
-# Calculating Yaw-axis angle of rotation
 HEIGHT = 480
 WIDTH = 640
 
 X_ORIGIN = WIDTH // 2
 Y_ORIGIN = HEIGHT // 2
 
+markerSize = 142
+
 camMtx = np.load("camera_matrix.npy")
 distCoeffs = np.load("dist_coeffs.npy")
 
+# Returns pose of the marker in the form of rotation and translation vectors.
 def my_estimatePoseSingleMarkers(corners, marker_size, mtx, distortion):
-    '''
-    This will estimate the rvec and tvec for each of the marker corners detected by:
-       corners, ids, rejectedImgPoints = detector.detectMarkers(image)
-    corners - is an array of detected corners for each detected marker in the image
-    marker_size - is the size of the detected markers
-    mtx - is the camera matrix
-    distortion - is the camera distortion matrix
-    RETURN list of rvecs, tvecs, and trash (so that it corresponds to the old estimatePoseSingleMarkers())
-    '''
     marker_points = np.array([[-marker_size / 2, marker_size / 2, 0],
                               [marker_size / 2, marker_size / 2, 0],
                               [marker_size / 2, -marker_size / 2, 0],
@@ -59,48 +63,51 @@ def arucoDetect(img):
     return detectedMarkers, ids, corners
 
 if __name__ == "__main__":
+    # read in single image
     img = cv.imread("-20deg.png")
-    detectedMarkers, ids, corners = arucoDetect(img)
     img = cv.undistort(img, camMtx, distCoeffs)
-    print(detectedMarkers)
-    print(ids)
-    print(corners)
+    detectedMarkers, ids, corners = arucoDetect(img)
+    # turn corners into np array
     corners1 = np.array(corners)
     corners1 = corners1.reshape(1, 4, 2)
     topLeft = corners1[0,1]
     topRight = corners1[0,0]
     bottomLeft = corners1[0,3]
     bottomRight = corners1[0,2]
+    # draw center of marker
     cX = int((topLeft[0] + topRight[0]) / 2.0)
     cY = int((topLeft[1] + bottomLeft[1]) / 2.0)
     cv.circle(img, (cX, cY), 4, (0,0,255), 2)
-
+    # draw center of image
     cv.circle(img, (X_ORIGIN, Y_ORIGIN),4, (0,0,255), 2)
-    print(cX)
-    print(cY)
-    markerSize = 142
+    
+
     rvec, tvec, markerPts = my_estimatePoseSingleMarkers(corners, markerSize, camMtx, distCoeffs)
     rvec = np.array(rvec)
     tvec = np.array(tvec)
+
+    # Draw pose using axes.
     cv.drawFrameAxes(img, camMtx, distCoeffs, rvec, tvec, markerSize * 1.5, 2)
     cv.aruco.drawDetectedMarkers(img, corners, ids)
     cv.imshow("target", img)
     cv.waitKey(0)
-
-    #vidCap = initializeCamera()
-    #sleep(.2)
+    
+    '''
+    Uncomment for live video feed.
+    vidCap = initializeCamera()
+    sleep(.2)
 
     # The first frame
-    #ret, frame = vidCap.read()
+    ret, frame = vidCap.read()
 
     # Show the first frame - ends when '0' is pushed
-    #cv.imshow('basicImg', frame)
-    #cv.waitKey(0)
-    #cv.destroyAllWindows()
-    #cv.imshow("Live Video", frame)
+    cv.imshow('basicImg', frame)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
+    cv.imshow("Live Video", frame)
 
     # While loop for LIVE video
-    '''
+    
     while vidCap.isOpened():
         # Read in camera data
         ret, frame = vidCap.read()
@@ -114,7 +121,23 @@ if __name__ == "__main__":
             cv.imshow("Live Video", grayscale)
             # If aruco detected, find the corners
             if detectedMarkers == True:
-                sleep(0.1)
+                corners1 = np.array(corners)
+                corners1 = corners1.reshape(1, 4, 2)
+                topLeft = corners1[0,1]
+                topRight = corners1[0,0]
+                bottomLeft = corners1[0,3]
+                bottomRight = corners1[0,2]
+                cX = int((topLeft[0] + topRight[0]) / 2.0)
+                cY = int((topLeft[1] + bottomLeft[1]) / 2.0)
+                cv.circle(img, (cX, cY), 4, (0,0,255), 2)
+
+                rvec, tvec, markerPts = my_estimatePoseSingleMarkers(corners, markerSize, camMtx, distCoeffs)
+                rvec = np.array(rvec)
+                tvec = np.array(tvec)
+
+                # Draw pose using axes.
+                cv.drawFrameAxes(img, camMtx, distCoeffs, rvec, tvec, markerSize * 1.5, 2)
+                cv.aruco.drawDetectedMarkers(img, corners, ids)
 
         # wait for 1ms
         cv.waitKey(1)
